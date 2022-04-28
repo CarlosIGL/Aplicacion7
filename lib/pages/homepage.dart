3972644d4366764b0750fccb552dac6f006908e0
CarlosIGL <al18760469@ite.edu.mx>
2022-04-28 08:19:30 -0700

    Actualizacion del proyecto

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:aplicacion7/pages/info.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,15 +13,21 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
 
-  final List<Estudiante> _personas = [
-    Estudiante(
-    "18760469", "Carlos Israel Godinez Luna", "Ing. Sistemas Computacionales",
-     "8vo","6462694189","al18760469@ite.edu.mx"
-    ),
-    Estudiante(
-        "16760180", "Virginia Rosario Ramirez Velasco", "Ing. Sistemas Computacionales",
-        "7mo","6461208790","al16760180@ite.edu.mx"),
-  ];
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) => leerJson(context));
+  }
+
+  List estudiantes = [];
+
+  Future<void> leerJson(BuildContext context) async{
+    final String datosLeidos =
+        await rootBundle.loadString('assets/estudiantes.json');
+    final datosDecodificados = await json.decode(datosLeidos);
+    setState(() {
+      estudiantes = datosDecodificados["estudiantes"];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,28 +36,28 @@ class _HomepageState extends State<Homepage> {
           title: const Text("Alumnos"),
         ),
         body: ListView.builder(
-            itemCount: _personas.length,
+            itemCount: estudiantes.length,
             itemBuilder: (context, index) {
 
 
               return ListTile(
 
-                title: Text(_personas[index].name),
-                subtitle: Text(_personas[index].numerocontrol),
+                title: Text(estudiantes[index]['name']),
+                subtitle: Text(estudiantes[index]['numerocontrol']),
                 leading:  CircleAvatar(
-                  child: Text(_personas[index].name.substring(0,1)),
+                  child: Text(estudiantes[index]['name'].substring(0,1)),
                 ),
                 trailing: const Icon(Icons.arrow_forward_ios),
 
                 onTap: () {
                   var route = MaterialPageRoute(
                     builder: (BuildContext context) => Info(
-                      value: _personas[index].numerocontrol.toString(),
-                      value2: _personas[index].name.toString(),
-                      value3: _personas[index].carrera.toString(),
-                      value4: _personas[index].semestre.toString(),
-                      value5: _personas[index].phone.toString(),
-                      value6: _personas[index].correo.toString(),
+                      value: estudiantes[index]['numerocontrol'].toString(),
+                      value2: estudiantes[index]['name'].toString(),
+                      value3: estudiantes[index]['carrera'].toString(),
+                      value4: estudiantes[index]['semestre'].toString(),
+                      value5: estudiantes[index]['phone'].toString(),
+                      value6: estudiantes[index]['correo'].toString(),
 
                     ),
                   );
@@ -59,15 +68,4 @@ class _HomepageState extends State<Homepage> {
             })
     );
   }
-
-}
-class Estudiante {
-  late String numerocontrol;
-  late String name;
-  late String carrera;
-  late String semestre;
-  late String phone;
-  late String correo;
-
-  Estudiante(this.numerocontrol, this.name, this.carrera, this.semestre, this.phone, this.correo);
 }
